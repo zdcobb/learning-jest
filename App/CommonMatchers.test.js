894f202d1e122132de9a3d2d2a6f8c9baf351724
Zachary Cobb <zdcobb@gmail.com>
2019-03-12 16:37:26 -0400

    First commit

---
+++ App/CommonMatchers.test.js
@@ -0,0 +1,5 @@
+let add = require('./add');
+
+test('2 Plus 2 is 4', () => {
+    expect(2 + 2).toBe(4);
+});
---
+++ sum.js
@@ -0,0 +1,5 @@
+// my first jest test
+function sum(a, b) {
+    return a + b;
+}
+module.exports = sum;
