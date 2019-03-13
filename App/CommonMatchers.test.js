2a489c0f4268587d57474889e6774d1ef299f14d
zdcobb <zcobb@bscales.com>
2019-03-12 20:38:40 -0400

    installed jest locally, added to gitignore

--- App/CommonMatchers.test.js
+++ App/CommonMatchers.test.js
@@ -3,3 +3,22 @@ let add = require('./CommonMatchers');
 test('2 Plus 2 is 4', () => {
     expect(2 + 2).toBe(4);
 });
+
+test('object assignment', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+});
+
+test('object 2 equality', () => {
+    let obj = {
+        prop1: 'First Property',
+        prop2: 'Second Property',
+        prop3: 3
+    }
+    expect(obj).toEqual({
+        prop1: 'First Property',
+        prop2: 'Second Property',
+        prop3: 3
+    });
+});
